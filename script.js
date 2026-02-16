982c0303eff03435c9914081c25d03f586cf441c
praveenraj-e5 <praveenraj.s@e5.ai>
2026-02-16 20:59:13 +0530

    Updated the image 2

--- script.js
+++ script.js
@@ -72,8 +72,8 @@
 
   // Every Moment with You: images from assets folder (browsers don't support .heic; use .jpeg/.jpg/.png)
   var EVERY_MOMENT_ASSETS_RAW = [
-    { path: "assets/Mirrir Selfi.jpg", caption: "Our first Mirror Selfi 📷" },
-    { path: "assets/First date.jpg", caption: "Our first Date 💕" },
+    { path: "assets/Mirrir Selfi.JPG", caption: "Our first Mirror Selfi 📷" },
+    { path: "assets/First date.JPG", caption: "Our first Date 💕" },
     { path: "assets/Ice Rink.jpg", caption: "Our first Outing 😉" },
     { path: "assets/First kiss.jpeg", caption: "Our first Kiss 😘" },
   ];
